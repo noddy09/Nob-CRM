159470da05f099ebcd135c4d7ba1c51c7dbd7e73
noddy09 <n.mhapadi@gmail.com>
2022-05-16 18:28:42 +0530

    - Initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <Navbar/>
+    
+    <section class="section">
+      <router-view/>
+    </section>
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  import Navbar from './components/layouts/Navbar';
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  export default{
+    name: 'App',
+    components: {
+      Navbar
     }
   }
+
+</script>
+
+<style lang="scss">
+@import '../node_modules/bulma';
+.is-orange {
+    background-color: #ffa500;
+    border-color: transparent;
+    color: #fff;
 }
 </style>
---
+++ src/components/layouts/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav class="navbar has-background-white-ter">
+        <div class="navbar-brand">
+            <router-link to="/" class="navbar-items">
+                <img class="imgLogo" src="../../assets/logo.png"/>
+            </router-link>
+        </div>
+
+        <div class="navbar-menu">
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <router-link to="/sign-up" class="button is-orange"><strong>Sign Up</strong></router-link>
+                        <router-link to="/login" class="button is-dark"><strong>Login</strong></router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: 'Navbar'
+    }
+</script>
+
+<style lang="css">
+.imgLogo{
+    margin-left: 30px;
+    height: 80px;
+    width: 100px;
+}
+</style>
